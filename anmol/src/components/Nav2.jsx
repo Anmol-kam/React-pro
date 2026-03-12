50325e4caa4b4f2280035b2265e17a43cb717a2d
anmol <singjila5@gmail.com>
2026-03-12 10:09:09 +0530

    ef

--- anmol/src/components/Nav2.jsx
+++ anmol/src/components/Nav2.jsx
@@ -23,12 +23,12 @@ export default function Nav2() {
 
   return (
  <>
- <nav className='bg-black text-white  flex justify-between p-4 relative '>
-    <div>
-        logo
+ <nav className='bg-black text-white  flex justify-between p-4 relative items-center'>
+    <div className='text-3xl '>
+        LOGO
     </div>
      
-     <ul  className='hidden md:flex gap-10'>
+     <ul  className='hidden md:flex gap-10 text-xl'>
       {
           menu.map((v,i)=>(
             <li key={i}><a href={v.href}>{v.Name}</a></li>
@@ -36,7 +36,7 @@ export default function Nav2() {
       }
         
      </ul>
-     <button className='hidden md:block' >LOG IN</button>
+     <button className='hidden md:block bg-blue-600 p-2 rounded-sm hover:bg-blue-800 text-xl' >LOG IN</button>
      <div className='block md:hidden' onClick={togg}>
         {
             open  ?  <RxCross2/> :<FaBars/> 
@@ -45,7 +45,7 @@ export default function Nav2() {
 
     {
         open && (
-            <ul className='bg-gray-950 text-white w-full left-0 absolute top-10 flex flex-col gap-2'>
+            <ul className='bg-gray-950 text-white w-full absolute top-10 flex flex-col left-0 gap-2 mt-7'>
                 {
                  menu.map((v,i)=>(
             <li key={i}><a href={v.href}>{v.Name}</a></li>
