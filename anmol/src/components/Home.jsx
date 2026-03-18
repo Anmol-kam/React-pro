fc08f5fe38f801451ea6d48053e3433385b37272
anmol <singjila5@gmail.com>
2026-03-18 12:44:10 +0530

    hugdw

--- anmol/src/components/Home.jsx
+++ anmol/src/components/Home.jsx
@@ -1,35 +1,58 @@
 import React from 'react'
 import { FaMobileAlt } from "react-icons/fa";
-import a1 from "../image/"
+import a1 from "../image/bg3.jpg"
 
 
 export default function Home() {
-const menu=[
-    {href:"/" , Name:"Home"},
-    {href:"/" , Name:"About"},
-    {href:"/" , Name:"Resume"},
-    {href:"/" , Name:"Portfolio"}
-]
+    const menu = [
+        { href: "/", Name: "Home" },
+        { href: "/", Name: "About" },
+        { href: "/", Name: "Resume" },
+        { href: "/", Name: "Portfolio" }
+    ]
 
-  return (
-    <>
-<nav className=' bg-black text-white flex justify-between h-15 p-8'>
-    <ul className='flex items-center gap-10 '>
-        {
-        menu.map((v,i)=>(
-        <li key={i}><a href={v.href}>{v.Name}</a></li>
-        ))
-        }
-    </ul>
-        
-        <button className='flex items-center gap-1.5'><FaMobileAlt /> 716-666-67435</button>
-    
-</nav>
+    return (
+        <>
+            <nav className=' bg-black/95 backdrop-blur-md text-white flex justify-between h-15 pl-10 pr-10 '>
+                <ul className='flex items-center gap-10 '>
+                    {
+                        menu.map((v, i) => (
+                            <li key={i}><a href={v.href}>{v.Name}</a></li>
+                        ))
+                    }
+                </ul>
 
-   <div>
-    <img src={a1} className='w-screen h-screen' />
-   </div>
+                <button className='flex items-center gap-1.5'><FaMobileAlt /> 716-666-67435</button>
 
-    </>
-  )
+            </nav>
+
+            <div className="relative w-full h-screen overflow-hidden">
+
+                <img
+                    src={a1}
+                    className="w-full h-full object-cover object-center contrast-110 brightness-105 saturate-110  [image-rendering:auto]"
+                    alt="background"
+                />
+
+                <div className="absolute inset-0 bg-black/40 backdrop-blur-[1px]"></div>
+
+
+                <div className="absolute top-1/2 left-20 transform -translate-y-1/2 text-white">
+                    <h1 className="text-7xl font-semibold">ANMOL</h1>
+                    <p className="text-3xl tracking-widest mt-2">Web Developer</p>
+
+                    <div className="mt-10 flex gap-10">
+                        <button className="border rounded-2xl px-5 py-1 text-xl  transition-all duration-300 hover:ring-2 hover:ring-blue-500 hover:border-transparent">
+                            Resume
+                        </button>
+
+                        <button className="border rounded-2xl px-5 py-1 text-xl transition-all duration-300 hover:ring-2 hover:ring-blue-500 hover:border-transparent">
+                            Portfolio
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
 }
